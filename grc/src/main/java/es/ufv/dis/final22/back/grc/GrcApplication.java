9004c852c748430e336f32567379da981a044381
guilleeeee5 <guillermo.ramirez@MacBook-Pro-83.local>
2023-01-20 16:46:33 +0100

    Inicializado proyecto Spring

---
+++ grc/src/main/java/es/ufv/dis/final22/back/grc/GrcApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final22.back.grc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrcApplication.class, args);
+	}
+
+}
---
+++ grc/src/test/java/es/ufv/dis/final22/back/grc/GrcApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final22.back.grc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
